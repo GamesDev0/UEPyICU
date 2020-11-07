9f569d9b75ad9317c8eb222d9ee17fc18353c8f1
kiop <nick028@foxmail.com>
2020-11-07 18:52:22 +0800

    update: add comment

--- scripts/1.py
+++ scripts/1.py
@@ -3,7 +3,8 @@ icu_lib = unreal.UEPyICUBPLibrary
 
 files = icu_lib.open_file_dialog("open dialog", ".", "*")
 # if utf8 string , encode
-print(files[0].encode('utf8'))
+file_name = files[0].encode('utf8')
+print(file_name)
 
-# print info on viewport
-icu_lib.log_info_on_screen("Hello , UEPyICU")
+# print info on viewport , this no needs encode
+icu_lib.log_info_on_screen(file_name)

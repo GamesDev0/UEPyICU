5ef8b59abab9d90280529cd736557e65a3626833
kiop <nick028@foxmail.com>
2020-11-07 17:58:58 +0800

    update: add plugin file

---
+++ Plugins/UEPyICU/Source/UEPyICU/Private/UEPyICUBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "UEPyICUBPLibrary.h"
+#include "UEPyICU.h"
+
+UUEPyICUBPLibrary::UUEPyICUBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UUEPyICUBPLibrary::UEPyICUSampleFunction(float Param)
+{
+	return -1;
+}
+
